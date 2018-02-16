c712126fe1120e72404018ad14f33bbb724ebc19
Salim Huneidi <salim@huneidi.com>
2018-02-16 18:36:08 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,6 +1,7 @@
 class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
+  
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
@@ -8,9 +9,17 @@ class HangpersonGame
   # def initialize()
   # end
   
+  
+   attr_accessor :word, :guesses, :wrong_guesses
+  
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
+
+    
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -23,5 +32,48 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+
+ def new word
+    @hangpersonGame = HangpersonGame.new(word)
+ end
+
+  def guess char
+    if char =~ /[[:alpha:]]/
+      char.downcase!
+      if @word.include? char and !@guesses.include? char
+        @guesses.concat char
+        return true
+      elsif !@wrong_guesses.include? char and !@word.include? char
+        @wrong_guesses.concat char
+        return true
+      else return false end
+    else
+      char = :invalid
+      raise ArgumentError
+    end
+  end
+
+  def word_with_guesses
+    result = ""
+    @word.each_char do |letter|
+      if @guesses.include? letter
+        result.concat letter 
+      else
+        result.concat '-'
+      end
+    end
+    return result
+  end
+
+  def check_win_or_lose
+    counter = 0
+    return :lose if @wrong_guesses.length >= 7
+    @word.each_char do |letter|
+      counter += 1 if @guesses.include? letter
+    end
+    if counter == @word.length then :win
+    else :play end
+  end
+
 
 end
