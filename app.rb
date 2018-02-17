71889349dbef53d1c4d4d648df9b41b50870003c
Salim Huneidi <salim@huneidi.com>
2018-02-17 12:31:58 +0000

    Part 1 done

--- app.rb
+++ app.rb
@@ -39,11 +39,11 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    
+    ### YOUR CODE HERE ###
     if letter.nil? || !(/[A-Za-z]/ === letter)
-      flash[:message] = "sorry!, Invalid Guess." 
+      flash[:message] = "Invalid guess." 
     else 
-      (@game.wrong_guesses[letter] || @game.guesses[letter])? flash[:message] = "Letter already used ." : @game.guess(letter)
+      (@game.wrong_guesses[letter] || @game.guesses[letter])? flash[:message] = "You have already used that letter." : @game.guess(letter)
     end
     redirect '/show'
   end
@@ -54,12 +54,12 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use instance variables
   # @wrong_guesses and @word_with_guesses, so set those up here.
   get '/show' do
-   
+    ### YOUR CODE HERE ###
     @game.check_win_or_lose == :play ? (erb :show): (@game.check_win_or_lose == :win ? (redirect '/win') : (redirect '/lose'))
   end
   
   get '/win' do
-   
+    ### YOUR CODE HERE ###
     if @game.check_win_or_lose == :win
       erb :win 
     else
@@ -68,7 +68,7 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/lose' do
-    
+    ### YOUR CODE HERE ###
     if @game.check_win_or_lose == :lose
       erb :lose 
     else
